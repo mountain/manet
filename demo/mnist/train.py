523c625e831c1ee36efbdb14ea39e6bf898e7273
Mingli Yuan <mingli.yuan@gmail.com>
2023-05-04 20:48:03 +0800

    retire lightning

--- demo/mnist/train.py
+++ demo/mnist/train.py
@@ -71,9 +71,9 @@ if __name__ == '__main__':
                                  ]))
 
     mnist_train, mnist_val = random_split(dataset, [55000, 5000])
-    train_loader = DataLoader(mnist_train, shuffle=True, batch_size=opt.batch, num_workers=0)
-    val_loader = DataLoader(mnist_val, batch_size=opt.batch, num_workers=0)
-    test_loader = DataLoader(mnist_val, batch_size=opt.batch, num_workers=0)
+    train_loader = DataLoader(mnist_train, shuffle=True, batch_size=opt.batch, num_workers=16)
+    val_loader = DataLoader(mnist_val, batch_size=opt.batch, num_workers=16)
+    test_loader = DataLoader(mnist_val, batch_size=opt.batch, num_workers=16)
 
     import importlib
     print('construct model...')
