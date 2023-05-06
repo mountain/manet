17ed8bd097f25b2bd6aa3c4c863d83900e3ba5be
Mingli Yuan <mingli.yuan@gmail.com>
2023-05-06 16:17:42 +0800

    typo

--- demo/mnist/mnist4.py
+++ demo/mnist/mnist4.py
@@ -47,7 +47,7 @@ class MNModel4(pl.LightningModule):
         dc = th.zeros_like(inputs)
         do = th.zeros_like(inputs)
         for _ in range(6):
-            state = th.sigmoid(self.ulearner(th.cat(context, inputs), dim=1)).view(-1, 8, 80)
+            state = th.sigmoid(self.ulearner(th.cat((context, inputs), dim=1))).view(-1, 8, 80)
             p, r, t, v = state[:, 0], state[:, 1], state[:, 2], state[:, 3]
             q, s, u, w = state[:, 4], state[:, 5], state[:, 6], state[:, 7]
             p, q = 4 * p, 4 * q
