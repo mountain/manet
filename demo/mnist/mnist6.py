292f87a2daf6094cace586c65050fcc0eac835e8
mingli <mingli.yuan@gmail.com>
2023-05-07 02:58:39 +0800

    add version 6

--- demo/mnist/mnist6.py
+++ demo/mnist/mnist6.py
@@ -29,7 +29,7 @@ class MNModel4(pl.LightningModule):
         dc = th.zeros_like(inputs)
         do = th.zeros_like(inputs)
         for _ in range(3):
-            state = th.sigmoid(learner(th.cat((context, inputs), dim=1))).view(-1, 8, 49 * 3)
+            state = th.sigmoid(learner(th.cat((context, inputs), dim=1))).view(-1, 8, 49 * 2)
             p, r, t, v = state[:, 0], state[:, 1], state[:, 2], state[:, 3]
             q, s, u, w = state[:, 4], state[:, 5], state[:, 6], state[:, 7]
             p, q = 4 * p, 4 * q
