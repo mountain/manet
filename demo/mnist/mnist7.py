b769ef7c7960ac91af6c1be6efbd358c821da557
mingli <mingli.yuan@gmail.com>
2023-05-10 09:34:17 +0800

    try matrix version

--- demo/mnist/mnist7.py
+++ demo/mnist/mnist7.py
@@ -13,23 +13,26 @@ class MNModel7(pl.LightningModule):
     def __init__(self, learning_rate=1e-3):
         super().__init__()
         self.learning_rate = 1e-3
+        self.counter = 0
+        self.labeled_loss = 0
+        self.labeled_correct = 0
         self.recognizer = nn.Sequential(
             nn.Conv2d(1, 3, kernel_size=5, padding=2),
             nn.MaxPool2d(2),
-            MLP(1, [1], mac_unit=MacMatrixUnit),
+            MLP(1, [1], mac_unit=MacMatrixUnit, mac_steps=6),
             Reshape(3, 14, 14),
             nn.Conv2d(3, 6, kernel_size=5, padding=2),
             nn.MaxPool2d(2),
-            MLP(1, [1], mac_unit=MacMatrixUnit),
+            MLP(1, [1], mac_unit=MacMatrixUnit, mac_steps=6),
             Reshape(6, 7, 7),
             nn.Conv2d(6, 12, kernel_size=5, padding=2),
             nn.MaxPool2d(2),
-            MLP(1, [1], mac_unit=MacMatrixUnit),
+            MLP(1, [1], mac_unit=MacMatrixUnit, mac_steps=6),
             Reshape(12, 3, 3),
             nn.Conv2d(12, 24, kernel_size=3, padding=1),
             nn.MaxPool2d(2),
             Reshape(24, 1),
-            MLP(24, [10], mac_unit=MacMatrixUnit),
+            MLP(24, [10], mac_unit=MacMatrixUnit, mac_steps=6),
             nn.LogSoftmax(dim=1)
         )
 
@@ -56,8 +59,9 @@ class MNModel7(pl.LightningModule):
         pred = z.data.max(1, keepdim=True)[1]
         correct = pred.eq(y.data.view_as(pred)).sum() / y.size()[0]
         self.log('correct', correct, prog_bar=True)
-        self.labeled_loss = loss.item()
-        self.labeled_correct = correct.item()
+        self.labeled_loss += loss.item() * y.size()[0]
+        self.labeled_correct += correct.item() * y.size()[0]
+        self.counter += y.size()[0]
 
     def test_step(self, test_batch, batch_idx):
         x, y = test_batch
@@ -70,13 +74,19 @@ class MNModel7(pl.LightningModule):
     def on_save_checkpoint(self, checkpoint) -> None:
         import pickle, glob, os
 
-        record = '%2.5f-%03d-%1.5f.ckpt' % (self.labeled_correct, checkpoint['epoch'], self.labeled_loss)
+        correct = self.labeled_correct / self.counter
+        loss = self.labeled_loss / self.counter
+        record = '%2.5f-%03d-%1.5f.ckpt' % (correct, checkpoint['epoch'], loss)
         fname = 'best-%s' % record
         with open(fname, 'bw') as f:
             pickle.dump(checkpoint, f)
-        for ix, ckpt in enumerate(sorted(glob.glob('best-*.ckpt'), reverse=True)):
-            if ix > 5:
-                os.unlink(ckpt)
+        # for ix, ckpt in enumerate(sorted(glob.glob('best-*.ckpt'), reverse=True)):
+        #     if ix > 5:
+        #         os.unlink(ckpt)
+
+        self.counter = 0
+        self.labeled_loss = 0
+        self.labeled_correct = 0
 
 
 def _model_():
--- demo/mnist/mnist8.py
+++ demo/mnist/mnist8.py
@@ -18,17 +18,17 @@ class MNModel8(pl.LightningModule):
             MLP(1, [1], mac_unit=MacMatrixUnit, mac_steps=6),
             Reshape(32, 14, 14),
             nn.Conv2d(32, 64, kernel_size=5, padding=2),
-            nn.AvgPool2d(2),
-            MLP(1, [1], mac_unit=MacMatrixUnit, mac_steps=4),
+            nn.MaxPool2d(2),
+            MLP(1, [1], mac_unit=MacMatrixUnit, mac_steps=6),
             Reshape(64, 7, 7),
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.AvgPool2d(2),
-            MLP(1, [1], mac_unit=MacMatrixUnit, mac_steps=4),
+            nn.MaxPool2d(2),
+            MLP(1, [1], mac_unit=MacMatrixUnit, mac_steps=6),
             Reshape(128, 3, 3),
             nn.Conv2d(128, 256, kernel_size=1, padding=0),
-            nn.AvgPool2d(2),
+            nn.MaxPool2d(2),
             Reshape(256, 1),
-            MLP(256, [10], mac_unit=MacMatrixUnit, mac_steps=4),
+            MLP(256, [10], mac_unit=MacMatrixUnit, mac_steps=6),
             nn.LogSoftmax(dim=1)
         )
 
