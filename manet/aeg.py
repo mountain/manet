5ba9a730e3c3d0a3bf31fe51ac80397da6de683d
Mingli Yuan <mingli.yuan@gmail.com>
2023-05-17 20:03:41 +0800

    add debug code

--- manet/aeg.py
+++ manet/aeg.py
@@ -164,8 +164,8 @@ class LearnableFunction(ExprFlow):
         sz = data.size()
 
         if self.debug and self.logger is not None:
-            self.logger.add_figure('%s:function:velocity:%d' % (self.debug_key, self.current_epoch), self.plot_function('velocity'), self.current_epoch)
-            self.logger.add_figure('%s:function:angles:%d' % (self.debug_key, self.current_epoch), self.plot_function('angles'), self.current_epoch)
+            self.logger.add_figure('%s:function:velocity:%d' % (self.debug_key, self.current_epoch), self.params.plot_function('velocity'), self.current_epoch)
+            self.logger.add_figure('%s:function:angles:%d' % (self.debug_key, self.current_epoch), self.params.plot_function('angles'), self.current_epoch)
             self.logger.add_histogram('%s:input:%d:0' % (self.debug_key, self.current_step), data[0], self.current_epoch)
             self.logger.add_histogram('%s:input:%d:1' % (self.debug_key, self.current_step), data[1], self.current_epoch)
             self.logger.add_histogram('%s:input:%d:2' % (self.debug_key, self.current_step), data[2], self.current_epoch)
