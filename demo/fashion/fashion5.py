ce8acf1c67d21e334e327bee2d61af213848cc23
Mingli Yuan <mingli.yuan@gmail.com>
2023-05-20 05:59:13 +0800

    try fashionmnist5

--- demo/fashion/fashion5.py
+++ demo/fashion/fashion5.py
@@ -18,9 +18,9 @@ class Fashion4(pl.LightningModule):
         self.labeled_correct = 0
         self.learnable_function0 = LogisticFunction(p=3.8, debug_key='lf0')
         self.learnable_function1 = LogisticFunction(p=3.8, debug_key='lf1')
-        self.learnable_function2 = LogisticFunction(p=3.8, debug_key='lf2')
+        self.learnable_function2 = LogisticFunction(p=3.9, debug_key='lf2')
         self.learnable_function3 = LogisticFunction(p=3.8, debug_key='lf3')
-        self.learnable_function4 = LogisticFunction(p=3.8, debug_key='lf4')
+        self.learnable_function4 = LogisticFunction(p=3.9, debug_key='lf4')
         self.learnable_function5 = LogisticFunction(p=3.8, debug_key='lf5')
         self.recognizer = nn.Sequential(
             nn.Conv2d(1, 5, kernel_size=5, padding=2),
