7acab156832b26840bb20aec31754f37c74e867a
Mingli Yuan <mingli.yuan@gmail.com>
2023-05-19 22:54:56 +0800

    add experiment by contrast

---
+++ demo/mnist/mnist9.py
@@ -0,0 +1,154 @@
+import torch as th
+import lightning as pl
+
+from torch import nn
+from torch.nn import functional as F
+from torchvision.ops import MLP
+
+from manet.aeg import LogisticFunction
+from manet.mac import Reshape
+
+
+class MNModel9(pl.LightningModule):
+    def __init__(self, learning_rate=1e-3):
+        super().__init__()
+        self.learning_rate = 1e-3
+        self.counter = 0
+        self.labeled_loss = 0
+        self.labeled_correct = 0
+        self.learnable_function0 = LogisticFunction(debug_key='lf0')
+        self.learnable_function1 = LogisticFunction(debug_key='lf1')
+        self.learnable_function2 = LogisticFunction(debug_key='lf2')
+        self.learnable_function3 = LogisticFunction(debug_key='lf3')
+        self.recognizer = nn.Sequential(
+            nn.Conv2d(1, 5, kernel_size=5, padding=2),
+            self.learnable_function0,
+            Reshape(5, 28, 28),
+            nn.MaxPool2d(2),
+            nn.Conv2d(5, 10, kernel_size=3, padding=1),
+            self.learnable_function1,
+            Reshape(10, 14, 14),
+            nn.MaxPool2d(2),
+            nn.Conv2d(10, 20, kernel_size=3, padding=1),
+            self.learnable_function2,
+            Reshape(20, 7, 7),
+            nn.MaxPool2d(2),
+            nn.Conv2d(20, 40, kernel_size=1, padding=0),
+            self.learnable_function3,
+            Reshape(40, 3, 3),
+            MLP(40 * 9, [10]),
+            Reshape(10),
+            nn.LogSoftmax(dim=1)
+        )
+
+        self.tb_logger = None
+        self.learnable_function0.logger = None
+        self.learnable_function1.logger = None
+        self.learnable_function2.logger = None
+        self.learnable_function3.logger = None
+
+    def forward(self, x):
+        return self.recognizer(x)
+
+    def configure_optimizers(self):
+        return th.optim.Adam(self.parameters(), lr=self.learning_rate)
+
+    def training_step(self, train_batch, batch_idx):
+        x, y = train_batch
+        x = x.view(-1, 1, 28, 28)
+        z = self(x)
+        loss = F.nll_loss(z, y)
+        self.log('train_loss', loss, prog_bar=True)
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        if batch_idx % 100 == 0:
+            self.learnable_function0.debug = True
+            self.learnable_function1.debug = True
+            self.learnable_function2.debug = True
+            self.learnable_function3.debug = True
+
+            self.learnable_function0.global_step += 1
+            self.learnable_function1.global_step += 1
+            self.learnable_function2.global_step += 1
+            self.learnable_function3.global_step += 1
+
+            import lightning.pytorch.loggers as pl_loggers
+            tb_logger = None
+            for logger in self.trainer.loggers:
+                if isinstance(logger, pl_loggers.TensorBoardLogger):
+                    tb_logger = logger.experiment
+                    break
+            if tb_logger is None:
+                raise ValueError('TensorBoard Logger not found')
+
+            self.tb_logger = tb_logger
+            self.learnable_function0.logger = tb_logger
+            self.learnable_function1.logger = tb_logger
+            self.learnable_function2.logger = tb_logger
+            self.learnable_function3.logger = tb_logger
+
+        x, y = val_batch
+        x = x.view(-1, 1, 28, 28)
+
+        if batch_idx % 100 == 0:
+            y_true = y[:self.learnable_function0.num_samples]
+            self.learnable_function0.labels = y_true
+            self.learnable_function1.labels = y_true
+            self.learnable_function2.labels = y_true
+            self.learnable_function3.labels = y_true
+
+        z = self(x)
+        loss = F.nll_loss(z, y)
+        self.log('val_loss', loss, prog_bar=True)
+        pred = z.data.max(1, keepdim=True)[1]
+        correct = pred.eq(y.data.view_as(pred)).sum() / y.size()[0]
+        self.log('correctness', correct, prog_bar=True)
+        self.labeled_loss += loss.item() * y.size()[0]
+        self.labeled_correct += correct.item() * y.size()[0]
+        self.counter += y.size()[0]
+
+        if batch_idx % 100 == 0:
+            imgs = x[:10]
+            y_true = y[:10]
+            y_pred = pred[:10]
+            self.log_tb_images((imgs, y_true, y_pred, self.learnable_function0.global_step))
+
+        self.learnable_function0.debug = False
+        self.learnable_function1.debug = False
+        self.learnable_function2.debug = False
+        self.learnable_function3.debug = False
+
+    def test_step(self, test_batch, batch_idx):
+        x, y = test_batch
+        x = x.view(-1, 1, 28, 28)
+        z = self(x)
+        pred = z.data.max(1, keepdim=True)[1]
+        correct = pred.eq(y.data.view_as(pred)).sum() / y.size()[0]
+        self.log('correct', correct, prog_bar=True)
+
+    def on_save_checkpoint(self, checkpoint) -> None:
+        import pickle, glob, os
+
+        correct = self.labeled_correct / self.counter
+        loss = self.labeled_loss / self.counter
+        record = '%2.5f-%03d-%1.5f.ckpt' % (correct, checkpoint['epoch'], loss)
+        fname = 'best-%s' % record
+        with open(fname, 'bw') as f:
+            pickle.dump(checkpoint, f)
+        for ix, ckpt in enumerate(sorted(glob.glob('best-*.ckpt'), reverse=True)):
+            if ix > 5:
+                os.unlink(ckpt)
+
+        self.counter = 0
+        self.labeled_loss = 0
+        self.labeled_correct = 0
+
+    def log_tb_images(self, viz_batch) -> None:
+        image, y_true, y_pred, idx = viz_batch
+        for img_idx, (img, ground, pred) in enumerate(zip(image, y_true, y_pred)):
+            self.tb_logger.add_image(f"Image/{idx}_{img_idx}-{ground.item()}-{pred.item()}", img, idx)
+
+
+def _model_():
+    return MNModel9()
--- manet/aeg.py
+++ manet/aeg.py
@@ -11,7 +11,6 @@ P = TypeVar('P', bound='Parametor')
 U = TypeVar('U', bound='UnaryFunction')
 F = TypeVar('F', bound='Flow')
 M = TypeVar('M', bound='MacMatrixUnit')
-P = TypeVar('P', bound='MLP')
 
 Accessor = Callable[[Tensor], Tensor]
 Mapper = Callable[[Tensor], Tensor]
@@ -29,14 +28,6 @@ class ExprFlow(nn.Module):
 
     def access(self: F, handler: Tensor) -> Tuple[Tensor, Tensor]:
         raise NotImplemented()
-
-    def reduce(self: F, data: Tensor) -> Tensor:
-        raise NotImplemented()
-
-    def evaluate(self: F, data: Tensor) -> Tensor:
-        handler = self.accessor(data)
-        velocity, angle = self.access(handler)
-        return data + velocity * th.cos(angle) + data * velocity * th.sin(angle)
 
     def forward(self: F, data: Tensor) -> Tensor:
         return self.reduce(data)
@@ -117,6 +108,27 @@ class PiecewiseLinearParam(Param):
         bgn, end, t = self.begin_end_of(handler)
         p0, p1 = param[bgn], param[end]
         return (1 - t) * p0 + t * p1
+
+
+class CubicHermiteParam(Param):
+    def __init__(self: P, module: Module, num_points: int = 5, initializers: Dict[str, Initializer] = None) -> None:
+        super().__init__(module, num_points, initializers)
+
+    def __call__(self: P, key: str, handler: Tensor) -> Tensor:
+        param = self._construct(key)
+        value, derivative = param[0], param[1]
+
+        bgn, end, t = self.begin_end_of(handler)
+        p0, p1 = value[bgn], value[end]
+        m0, m1 = derivative[bgn], derivative[end]
+
+        # Cubic Hermite spline
+        q1 = (2 * t ** 3 - 3 * t ** 2 + 1) * p0
+        q2 = (t ** 3 - 2 * t ** 2 + t) * m0
+        q3 = (-2 * t ** 3 + 3 * t ** 2) * p1
+        q4 = (t ** 3 - t ** 2) * m1
+
+        return q1 + q2 + q3 + q4
 
 
 class CubicHermiteParam(Param):
@@ -243,3 +255,70 @@ class LearnableFunction(ExprFlow):
         #             self.logger.add_histogram('%s:output:%d' % (self.debug_key, self.labels[ix]), data[ix], self.global_step)
 
         return data / self.maxval
+
+
+class LogisticFunction(ExprFlow):
+
+    def __init__(self: U, num_steps: int = 3, debug: bool = False, debug_key: str = None, logger: TensorBoardLogger = None) -> None:
+        super().__init__()
+        self.num_steps = num_steps
+        self.p = nn.Parameter(th.ones(1).view(1, 1))
+        self.channel_transform = nn.Parameter(th.normal(0, 1, (1, 1)))
+        self.spatio_transform = nn.Parameter(th.normal(0, 1, (1, 1)))
+
+        self.debug = debug
+        self.debug_key = debug_key
+        self.logger = logger
+        self.global_step = 0
+        self.labels = None
+        self.num_samples = 20
+
+    def plot_total_function(self: F) -> Tensor:
+        line = th.linspace(0, self.num_points, 1000)
+        curve = self.p * line * (1 - line)
+
+        import matplotlib.pyplot as plt
+        x, y = line.detach().cpu().numpy(), curve.detach().cpu().numpy()
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(x, y)
+        return fig
+
+    def forward(self: F, data: Tensor) -> Tensor:
+        sz = data.size()
+
+        if self.debug and self.logger is not None:
+            if sz[0] > self.num_samples:
+                self.logger.add_figure('%s:0:function:total' % self.debug_key, self.plot_total_function(), self.global_step)
+
+        data = data.view(-1, sz[1], sz[2] * sz[3])
+        data = th.permute(data, [0, 2, 1]).reshape(-1, 1)
+        data = th.matmul(data, self.channel_transform)
+        data = data.view(-1, sz[2] * sz[3], sz[1])
+
+        if self.debug and self.logger is not None:
+            if sz[0] > self.num_samples:
+                for ix in range(self.num_samples):
+                    self.logger.add_histogram('%s:1:before:%d:histo' % (self.debug_key, self.labels[ix]), data[ix], self.global_step)
+                    image = data[ix].view(sz[2], sz[3] * sz[1])
+                    image = (image - image.min()) / (image.max() - image.min())
+                    self.logger.add_image('%s:1:before:%d:image' % (self.debug_key, self.labels[ix]), image, self.global_step, dataformats='HW')
+
+        data = th.sigmoid(data)
+        for ix in range(self.num_steps):
+            data = self.p * data * (1 - data)
+
+        if self.debug and self.logger is not None:
+            if sz[0] > self.num_samples:
+                for ix in range(self.num_samples):
+                    self.logger.add_histogram('%s:2:after:%d:histo' % (self.debug_key, self.labels[ix]), data[ix], self.global_step)
+                    image = data[ix].view(sz[2], sz[3] * sz[1])
+                    image = (image - image.min()) / (image.max() - image.min())
+                    self.logger.add_image('%s:2:after:%d:image' % (self.debug_key, self.labels[ix]), image, self.global_step, dataformats='HW')
+
+        data = data.view(-1, sz[2] * sz[3], sz[1])
+        data = th.permute(data, [0, 2, 1]).reshape(-1, 1)
+        data = th.matmul(data, self.spatio_transform)
+        data = data.view(*sz)
+
+        return data
