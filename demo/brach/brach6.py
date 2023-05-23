3eaa7d5d727ed01fb7826eacb676e6846ecfc90e
Mingli Yuan <mingli.yuan@gmail.com>
2023-05-23 23:56:55 +0800

    add mlp from torchvision

--- demo/brach/brach6.py
+++ demo/brach/brach6.py
@@ -14,7 +14,7 @@ class BRModel6(TraceNet):
         pass
 
     def trace(self, width, x, y):
-        return self.mlp(th.cat((x, y), dim=1).view(-1, 2, 1)).view(-1, 1, 1)
+        return self.mlp(th.cat((x, y), dim=1).view(-1, 2)).view(-1, 1, 1)
 
 
 def _model_():
