0c2dca4d898e05096b55e3795ab81ed101561c94
Mingli Yuan <mingli.yuan@gmail.com>
2023-05-25 15:34:09 +0800

    try stp

--- demo/brach/brach3.py
+++ demo/brach/brach3.py
@@ -8,16 +8,19 @@ class BRModel3(TraceNet):
     def __init__(self):
         super().__init__()
         self.a = LearnableFunction(in_channel=1, out_channel=1)
-        self.t = LearnableFunction(in_channel=1, out_channel=1)
+        self.t = LearnableFunction(in_channel=3, out_channel=1)
 
     def init(self, width, x, y):
         pass
 
     def trace(self, width, x, y):
-        a = self.a(width.view(-1, 1, 1)).view(-1, 1, 1)
-        t = self.t(x.view(-1, 1, 1)).view(-1, 1, 1)
-        y = 2 - a * (t - th.sin(t))
-        return y
+        w = width.view(-1, 1, 1)
+        x = x.view(-1, 1, 1)
+        y = y.view(-1, 1, 1)
+        a = self.a(w).view(-1, 1, 1)
+        t = self.t(th.cat((w, x, y), dim=1))
+        y_hat = 2 - a * (t - th.sin(t))
+        return y_hat
 
 
 def _model_():
