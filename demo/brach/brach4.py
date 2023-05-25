f3bd085ff6c58a5a43f4d5e10855c7786fb0d0fe
Mingli Yuan <mingli.yuan@gmail.com>
2023-05-25 16:48:48 +0800

    try stp

--- demo/brach/brach4.py
+++ demo/brach/brach4.py
@@ -1,4 +1,5 @@
 import torch as th
+from torch import nn
 
 from demo.brach.model import TraceNet
 from manet.aeg.flow import LearnableFunction
@@ -7,6 +8,7 @@ from manet.aeg.flow import LearnableFunction
 class BRModel3(TraceNet):
     def __init__(self):
         super().__init__()
+        self.g = nn.Parameter(th.ones(1, 1))
         self.a = LearnableFunction(in_channel=1, out_channel=1)
         self.t = LearnableFunction(in_channel=3, out_channel=1)
 
@@ -23,14 +25,17 @@ class BRModel3(TraceNet):
         y_1 = 2 - a * (t_1 - th.sin(t_1))
         t_2 = self.t(th.cat((w, x_1, y_1), dim=1))
         x_2 = a * (1 - th.cos(t_2))
-        y_2 = 2 - a * (t_1 - th.sin(t_2))
-        vx = (x_2 - x_1) / (t_2 - t_1)
-        vy = (y_2 - y_1) / (t_2 - t_1)
-        t_0_x = t_1 - (x_1 - x_0) / vx
-        t_0_y = t_1 - (y_1 - y_0) / vy
-        t_0 = (t_0_x + t_0_y) / 2
+        y_2 = 2 - a * (t_2 - th.sin(t_2))
 
-        return y_0 + (t_1 - t_0) * vy
+        dt = t_2 - t_1
+        vx = (x_2 - x_1) / dt
+        vy = (y_2 - y_1) / dt
+        k_1 = vx * vx + vy * vy
+        u_1 = self.g * y_1
+        e = k_1 + u_1
+
+        dy = th.sqrt((e - self.g * y_0) * dt * dt - (x_1 - x_0) * (x_1 - x_0))
+        return (y_0 + dy) * (y_2 > y_1) + (y_0 - dy) * (y_1 > y_2)
 
 
 def _model_():
