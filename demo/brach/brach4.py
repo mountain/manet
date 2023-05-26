a55f3366809b4211cba2e07ebac419a70958fcc9
Mingli Yuan <mingli.yuan@gmail.com>
2023-05-27 00:49:13 +0800

    try stp

--- demo/brach/brach4.py
+++ demo/brach/brach4.py
@@ -1,26 +1,19 @@
 import torch as th
 
 from demo.brach.model import TraceNet
-from manet.mac import MLP
+from manet.mac import MLP, MacSplineUnit
 
 
 class BRModel4(TraceNet):
     def __init__(self):
         super().__init__()
-        self.r = MLP(1, [1])
-        self.t = MLP(3, [1])
+        self.mlp = MLP(3, [1], mac_unit=MacSplineUnit)
 
     def init(self, width, x, y):
         pass
 
     def trace(self, width, x, y):
-        w = width.view(-1, 1, 1)
-        x = x.view(-1, 1, 1)
-        y = y.view(-1, 1, 1)
-        r = self.r(w).view(-1, 1, 1)
-        t = self.t(th.cat((w, x, y), dim=1).view(-1, 3, 1)).view(-1, 1, 1)
-        y_hat = 2 - r * (1 - th.cos(t))
-        return y_hat
+        return self.mlp(th.cat((x, y, width), dim=1).view(-1, 3, 1)).view(-1, 1, 1)
 
 
 def _model_():
