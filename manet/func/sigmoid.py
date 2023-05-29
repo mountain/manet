a5cd5579ab82bcea2e0d85aad3f752d145ea2ebe
Mingli Yuan <mingli.yuan@gmail.com>
2023-05-29 08:25:13 +0800

    fix param by phi

---
+++ manet/func/sigmoid.py
@@ -0,0 +1,58 @@
+import numpy as np
+import torch as th
+from torch import Tensor
+
+###
+# Sigmoid functions
+#
+# The sigmoid functions are used to map the output of the network to a value between 0 and 1.
+#
+# The sigmoid functions are ordered by their curve steepness. The first is the most steep one.
+#   - algb_1: Algebraic sigmoid function
+#   - natan: Normalized arctangent
+#   - algb_2: Algebraic sigmoid function
+#   - ngd: Normalized Gudermannian function
+#   - tanh: hyperbolic tangent
+#   - nerf: Normalized error function
+
+
+
+def algb_1(x: float | Tensor) -> float | Tensor:
+    return x / (1 + th.abs(x)) + 0.5
+
+
+def natan(x: float | Tensor) -> float | Tensor:
+    return th.atan(x * th.pi / 2) / th.pi * 2 + 0.5
+
+
+def algb_2(x: float | Tensor) -> float | Tensor:
+    return x / th.sqrt(1 + x * x) + 0.5
+
+
+def gd(x: float | Tensor) -> float | Tensor:
+    return 2 * th.atan(th.tanh(x / 2))
+
+
+def ngd(x: float | Tensor) -> float | Tensor:
+    return gd(x * th.pi / 2) / th.pi * 2 + 0.5
+
+
+def ntanh(x: float | Tensor) -> float | Tensor:
+    return th.tanh(x) + 0.5
+
+
+c = np.sqrt(np.pi) / 2
+
+
+def nerf(x: float | Tensor) -> float | Tensor:
+    return th.erf(x * c) + 0.5
+
+
+functions = {
+    'algb1': algb_1,
+    'natan': natan,
+    'algb2': algb_2,
+    'ngd': ngd,
+    'ntanh': ntanh,
+    'nerf': nerf
+}
