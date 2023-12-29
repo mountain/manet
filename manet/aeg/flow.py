24a0b832dec6cf272de29fb890852954a2d0cb20
Mingli Yuan <mingli.yuan@gmail.com>
2023-12-29 15:19:39 +0800

    by triple strategy

--- manet/aeg/flow.py
+++ manet/aeg/flow.py
@@ -23,7 +23,6 @@ class LearnableFunction(IterativeMap, Profiler):
 
         self.in_channel = in_channel
         self.out_channel = out_channel
-        # self.ch_transform = nn.Parameter(th.normal(0, 1, (out_channel, in_channel)))
         self.ch_transform = nn.Parameter(th.normal(0, 1, (in_channel, out_channel)))
         self.sp_transform = nn.Parameter(th.normal(0, 1, (1, 1)))
 
@@ -50,8 +49,6 @@ class LearnableFunction(IterativeMap, Profiler):
 
     @plot_iterative_function(dkey)
     def before_forward(self: Lf, data: Tensor) -> Tensor:
-        # data = th.matmul(self.ch_transform, data)
-
         data = data.view(-1, self.in_channel, 1)
         data = th.permute(data, [0, 2, 1]).reshape(-1, self.in_channel)
         data = th.matmul(data, self.ch_transform)
@@ -77,8 +74,6 @@ class LearnableFunction(IterativeMap, Profiler):
         return data.view(*self.size) / self.maxval
 
     def after_forward(self: Lf, data: Tensor) -> Tensor:
-        # data = th.matmul(data, self.sp_transform)
-
         data = th.permute(data, [0, 2, 1]).reshape(-1, 1)
         data = th.matmul(data, self.sp_transform)
         data = data.view(-1, self.out_channel, 1)
