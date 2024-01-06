23b930e2e7ab6c619a5459bc3c3456fc7e4c3ea3
Mingli Yuan <mingli.yuan@gmail.com>
2024-01-06 20:06:15 +0800

    add more cases

---
+++ demo/fashion0/fashion0.py
@@ -0,0 +1,139 @@
+import torch as th
+import torch.nn.functional as F
+import torch.nn as nn
+import manet.func.sigmoid as sgmd
+
+from manet.nn.model import MNISTModel
+from torch import Tensor
+from typing import TypeVar, Tuple
+
+
+U = TypeVar('U', bound='Unit')
+
+
+class LNon(nn.Module):
+    def __init__(self: U,
+                 steps: int = 3,
+                 length: float = 0.33333,
+                 points: int = 5,
+                 ) -> None:
+        super().__init__()
+
+        self.num_steps = steps
+        self.step_length = length
+        self.num_points = points
+
+        self.theta = nn.Parameter(
+            th.linspace(0, 2 * th.pi, points).view(1, 1, points)
+        )
+        self.velocity = nn.Parameter(
+            th.linspace(0, 1, points).view(1, 1, points)
+        )
+        self.channel_transform = nn.Parameter(
+            th.normal(0, 1, (1, 1, 1))
+        )
+        self.spatio_transform = nn.Parameter(
+            th.normal(0, 1, (1, 1, 1))
+        )
+
+    def accessor(self: U,
+                 data: Tensor,
+                 ) -> Tuple[Tensor, Tensor, Tensor]:
+
+        index = th.sigmoid(data) * self.num_points
+
+        bgn = index.floor().long()
+        bgn = bgn * (bgn >= 0)
+        bgn = bgn * (bgn <= self.num_points - 2) + (bgn - 1) * (bgn > self.num_points - 2)
+        bgn = bgn * (bgn <= self.num_points - 2) + (bgn - 1) * (bgn > self.num_points - 2)
+        end = bgn + 1
+
+        return index, bgn, end
+
+    def access(self: U,
+               memory: Tensor,
+               accessor: Tuple[Tensor, Tensor, Tensor]
+               ) -> Tensor:
+
+        index, bgn, end = accessor
+        pos = index - bgn
+        memory = memory.flatten(0)
+        return (1 - pos) * memory[bgn] + pos * memory[end]
+
+    def step(self: U,
+             data: Tensor
+             ) -> Tensor:
+
+        accessor = self.accessor(data)
+        theta = self.access(self.theta, accessor)
+        velo = self.access(self.velocity, accessor)
+
+        # by the flow equation of the arithmetic expression geometry
+        ds = velo * self.step_length
+        dx = ds * th.cos(theta)
+        dy = ds * th.sin(theta)
+        val = data * (1 + dy) + dx
+        return val
+
+    def forward(self: U,
+                data: Tensor
+                ) -> Tensor:
+        shape = data.size()
+        data = data.flatten(1)
+        data = data.contiguous()
+        data = data.view(-1, 1, 1)
+
+        data = th.permute(data, [0, 2, 1]).reshape(-1, 1)
+        data = th.matmul(data, self.channel_transform)
+        # data = data * self.channel_transform
+        data = data.view(-1, 1, 1)
+
+        for ix in range(self.num_steps):
+            data = self.step(data)
+
+        data = th.permute(data, [0, 2, 1]).reshape(-1, 1)
+        data = th.matmul(data, self.spatio_transform)
+        # data = data * self.spatio_transform
+        data = data.view(-1, 1, 1)
+
+        return data.view(*shape)
+
+# accuracy: 0.89850
+# best-0.89850-058-0.29467.ckpt
+
+class Fashion0(MNISTModel):
+    def __init__(self):
+        super().__init__()
+        self.conv0 = nn.Conv2d(1, 5, kernel_size=7, padding=3)
+        self.lnon0 = LNon(steps=1, length=1, points=3)
+        self.conv1 = nn.Conv2d(5, 25, kernel_size=3, padding=1)
+        self.lnon1 = LNon(steps=1, length=1, points=3)
+        self.conv2 = nn.Conv2d(25, 25, kernel_size=1, padding=0)
+        self.lnon2 = LNon(steps=1, length=1, points=3)
+        self.conv3 = nn.Conv2d(25, 25, kernel_size=1, padding=0)
+        self.lnon3 = LNon(steps=1, length=1, points=3)
+        self.fc = nn.Linear(25 * 9, 10)
+
+    def forward(self, x):
+        x = self.conv0(x)
+        x = self.lnon0(x)
+        x = F.max_pool2d(x, 2)
+        x = self.conv1(x)
+        x = self.lnon1(x)
+        x = F.max_pool2d(x, 2)
+        x = self.conv2(x)
+        x = self.lnon2(x)
+        x = F.max_pool2d(x, 2)
+        x = self.conv3(x)
+        x = self.lnon3(x)
+        x = x.flatten(1)
+        x = self.fc(x)
+        x = F.log_softmax(x, dim=1)
+        return x
+
+    def configure_optimizers(self):
+        return [th.optim.AdamW(self.parameters(), lr=self.learning_rate)]
+
+
+def _model_():
+    return Fashion0()
---
+++ demo/fashion0/train.py
@@ -0,0 +1,87 @@
+import argparse
+import torch
+import lightning.pytorch as pl
+
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", "--n_epochs", type=int, default=500, help="number of epochs of training")
+parser.add_argument("-b", "--batch", type=int, default=32, help="batch size of training")
+parser.add_argument("-m", "--model", type=str, default='fashion2', help="model to execute")
+opt = parser.parse_args()
+
+if torch.cuda.is_available():
+    accelerator = 'gpu'
+elif torch.backends.mps.is_available():
+    accelerator = 'cpu'
+else:
+    accelerator = 'cpu'
+
+
+if __name__ == '__main__':
+
+    print('loading data...')
+    from torch.utils.data import DataLoader
+    from torch.utils.data import random_split
+    from torchvision.datasets import FashionMNIST
+    from torchvision import transforms
+
+
+    mnist_raw = FashionMNIST('datasets', train=True, download=True, transform=transforms.Compose([
+                                   transforms.ToTensor()
+                                 ]))
+    raw_loader = DataLoader(mnist_raw, shuffle=False, batch_size=opt.batch, num_workers=8)
+    counter, total, vairance = 0, 0, 0
+    for raw_batch in raw_loader:
+        x, y = raw_batch
+        x = x.view(-1, 1, 28, 28)
+        counter += (28 * 28 * x.shape[0])
+        total += x.sum()
+
+    mean = (total / counter).item()
+    for raw_batch in raw_loader:
+        x, y = raw_batch
+        x = x.view(-1, 1, 28, 28)
+        vairance += ((x - mean).pow(2)).sum()
+
+    std = torch.sqrt(vairance / counter).item()
+    print('mean: %f, std: %f' % (mean, std))
+
+    mnist_train = FashionMNIST('datasets', train=True, download=True, transform=transforms.Compose([
+                                   transforms.ToTensor(),
+                                   transforms.Normalize(
+                                     (mean,), (std,))
+                                 ]))
+
+    mnist_test = FashionMNIST('datasets', train=False, download=True, transform=transforms.Compose([
+                                   transforms.ToTensor(),
+                                   transforms.Normalize(
+                                     (mean,), (std,))
+                                 ]))
+
+
+    train_loader = DataLoader(mnist_train, shuffle=True, batch_size=opt.batch, num_workers=8, persistent_workers=True)
+    val_loader = DataLoader(mnist_test, batch_size=opt.batch, num_workers=8, persistent_workers=True)
+    test_loader = DataLoader(mnist_test, batch_size=opt.batch, num_workers=8, persistent_workers=True)
+
+    # training
+    print('construct trainer...')
+    trainer = pl.Trainer(max_epochs=opt.n_epochs, log_every_n_steps=1,
+                         callbacks=[EarlyStopping(monitor="correct_rate", mode="max", patience=30)])
+
+    import importlib
+    print('construct model...')
+    mdl = importlib.import_module('demo.fashion0.%s' % opt.model, package=None)
+    model = mdl._model_()
+
+    # fname = 'seed.ckpt'
+    # with open(fname, 'rb') as f:
+    #     import pickle
+    #     checkpoint = pickle.load(f)
+    #     model.load_state_dict(checkpoint['state_dict'], strict=False)
+
+    print('training...')
+    trainer.fit(model, train_loader, val_loader)
+
+    print('testing...')
+    trainer.test(model, test_loader)
