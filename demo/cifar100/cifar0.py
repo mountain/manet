a841caf867fa5cd0b7f865f107679a78cc1d278f
mingli <mingli.yuan@gmail.com>
2024-01-08 05:57:51 +0800

    try another indexing trick

--- demo/cifar100/cifar0.py
+++ demo/cifar100/cifar0.py
@@ -54,7 +54,6 @@ class LNon(nn.Module):
         frame, index = accessor
         frame = frame.view(1, 1, -1)
         index = index.view(1, 1, -1)
-        param = param.view(1, 1, -1)
         th.addcmul(frame, th.zeros_like(param), param, out=param)
 
         begin = (index.floor().long()) * (index < param.size(0) - 1).long() * (index >= 0).long()
