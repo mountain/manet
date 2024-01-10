2e435ae7ef752459269613dc3c5329ebf8ae80cd
Mingli Yuan <mingli.yuan@gmail.com>
2024-01-10 16:18:32 +0800

    add cifar

--- demo/fashion0/fashion0.py
+++ demo/fashion0/fashion0.py
@@ -12,17 +12,14 @@ U = TypeVar('U', bound='Unit')
 
 
 class LNon(nn.Module):
-    def __init__(self: U,
-                 groups: int = 1,
-                 points: int = 5,
-                 ) -> None:
+    def __init__(self: U, groups: int = 1, points: int = 120) -> None:
         super().__init__()
 
         self.groups = groups
         self.points = points
 
         theta = th.cat([th.linspace(0, 2 * th.pi, points).view(1, 1, points) for _ in range(groups)], dim=1)
-        velocity = th.cat([th.linspace(0, 1, points).view(1, 1, points) for _ in range(groups)], dim=1)
+        velocity = th.cat([th.linspace(0, 3, points).view(1, 1, points) for _ in range(groups)], dim=1)
         self.params = nn.Parameter(th.cat([theta, velocity], dim=0))
 
         self.channel_transform = nn.Parameter(
@@ -32,70 +29,100 @@ class LNon(nn.Module):
             th.normal(0, 1, (1, 1, 1, 1))
         )
 
-    def accessor(self: U,
-                 data: Tensor,
-                 param: Tensor,
-                 ) -> Tuple[Tensor, Tensor]:
-        data = data.flatten(0)
-        param = param.flatten(0)
-
-        dmax, dmin = data.max().item(), data.min().item()
-        prob, grid = th.histogram(data, bins=self.points, range=(dmin, dmax), density=True)
-        prob = prob / prob.sum()
-        accum = th.cumsum(prob, dim=0) * (self.points - 1)
-        grid = (grid[1:] + grid[:-1]) / 2
-
-        import manet.func.interp as interp
-        index = interp.interp1d(grid, accum, data)
-        frame = interp.interp1d(accum, param, th.arange(self.points))
-
-        return frame, index
-
     @staticmethod
-    def access(accessor: Tuple[Tensor, Tensor]) -> Tensor:
+    def by_minmax(param, data):
+        points = param.size(-1)
+        shape = data.size()
+        data_ = data.flatten(0)
+        param_ = param.flatten(0)
 
-        frame, index = accessor
-        frame = frame.view(1, 1, -1)
-        index = index.view(-1)
+        dmax, dmin = data_.max().item(), data_.min().item()
+        data_ = (data_ - dmin) / (dmax - dmin + 1e-8) * (points - 1)
+        index = data_.floor().long()
+        frame = param_
 
         begin = index.floor().long()
+        begin = begin.clamp(0, param.size(1) - 1)
         pos = index - begin
         end = begin + 1
-        end = end.clamp(0, frame.size(2) - 1)
+        end = end.clamp(0, param.size(1) - 1)
 
-        return (1 - pos) * frame[:, :, begin] + pos * frame[:, :, end]
+        result = (1 - pos) * frame[begin] + pos * frame[end]
 
-    def step(self: U, data: Tensor, param: Tensor) -> Tensor:
+        return result.view(*shape)
 
-        accessor = self.accessor(data, param[0:1])
-        theta = self.access(accessor).reshape(*data.size())
-        accessor = self.accessor(data, param[1:2])
-        velo = self.access(accessor).reshape(*data.size())
+    @staticmethod
+    def by_sigmoid(param, data):
+        points = param.size(-1)
+        shape = data.size()
+        data_ = data.flatten(0)
+        param_ = param.flatten(0)
 
+        index = th.sigmoid(data_) * (points - 1)
+        frame = param_
+
+        begin = index.floor().long()
+        begin = begin.clamp(0, param.size(1) - 1)
+        pos = index - begin
+        end = begin + 1
+        end = end.clamp(0, param.size(1) - 1)
+
+        result = (1 - pos) * frame[begin] + pos * frame[end]
+
+        return result.view(*shape)
+
+    @staticmethod
+    def by_dynamic(param, data):
+        import manet.func.interp as interp
+
+        points = param.size(-1)
+        shape = data.size()
+        data_ = data.flatten(0)
+        param_ = param.flatten(0)
+
+        dmax, dmin = data_.max().item(), data_.min().item()
+        prob, grid = th.histogram(data_, bins=points, range=(dmin, dmax), density=True)
+        prob = prob / prob.sum()
+        accum = th.cumsum(prob, dim=0) * (points - 1)
+        grid = (grid[1:] + grid[:-1]) / 2
+
+        index = interp.interp1d(grid, accum, data_)
+        frame = interp.interp1d(accum, param_, th.arange(points))
+
+        begin = index.floor().long()
+        begin = begin.clamp(0, frame.size(1) - 1)
+        pos = index - begin
+        end = begin + 1
+        end = end.clamp(0, frame.size(1) - 1)
+
+        result = (1 - pos) * frame[:, begin] + pos * frame[:, end]
+
+        return result.view(*shape)
+
+    def foilize(self: U, data: Tensor, param: Tensor) -> Tensor:
+
+        theta = self.by_minmax(param[0:1], data)
+        velo = self.by_minmax(param[1:2], data)
         ds = velo * 0.01
         dx = ds * th.cos(theta)
         dy = ds * th.sin(theta)
         val = data * (1 + dy) + dx
-
         return val
 
-    def forward(self: U,
-                data: Tensor
-                ) -> Tensor:
+    def forward(self: U, data: Tensor) -> Tensor:
         shape = data.size()
         data = data.contiguous()
-
         data = data * self.channel_transform
 
         trunk = []
+        params = self.params * th.ones_like(self.params)
         for ix in range(self.groups):
             data_slice = data[:, ix::self.groups].reshape(-1, 1, 1)
-            param_slice = self.params[:, ix:ix+1]
-            trunk.append(self.step(data_slice, param_slice))
+            param_slice = params[:, ix:ix+1]
+            trunk.append(self.foilize(data_slice, param_slice))
         data = th.cat(trunk, dim=1)
 
         data = data * self.spatio_transform
-
         return data.view(*shape)
 
 # accuracy: 0.89850
