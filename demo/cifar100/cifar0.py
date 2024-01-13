51ed5637fa586391f3721bfba20dd86b51d87fcb
Mingli Yuan <mingli.yuan@gmail.com>
2024-01-13 16:38:20 +0800

    fix resnet by reuse from torch

--- demo/cifar100/cifar0.py
+++ demo/cifar100/cifar0.py
@@ -10,75 +10,68 @@ from typing import TypeVar, Tuple
 U = TypeVar('U', bound='Unit')
 
 
-class Foil(nn.Module):
-    def __init__(self: U, groups: int = 1, points: int = 120) -> None:
+class LNon(nn.Module):
+
+    def __init__(self, points=5):
         super().__init__()
-
-        self.groups = groups
         self.points = points
+        self.iscale = nn.Parameter(th.normal(0, 1, (1, 1, 1, 1)))
+        self.oscale = nn.Parameter(th.normal(0, 1, (1, 1, 1, 1)))
+        self.theta = th.linspace(-th.pi, th.pi, points)
+        self.velocity = th.linspace(0, 3, points)
+        self.weight = nn.Parameter(th.normal(0, 1, (points, points)))
 
-        theta = th.cat([th.linspace(-th.pi, th.pi, points).view(1, 1, points) for _ in range(groups)], dim=1)
-        velocity = th.cat([th.linspace(-3, 3, points).view(1, 1, points) for _ in range(groups)], dim=1)
-        self.params = th.cat([theta, velocity], dim=0)
+    @th.compile
+    def integral(self, param, index):
+        return th.sum(param[index].view(-1, 1) * th.softmax(self.weight, dim=1)[index, :], dim=1)
 
-    @staticmethod
-    def by_tanh(param, data):
-        points = param.size(-1)
+    @th.compile
+    def interplot(self, param, index):
+        lmt = param.size(0) - 1
+
+        p0 = index.floor().long()
+        p1 = p0 + 1
+        pos = index - p0
+        p0 = p0.clamp(0, lmt)
+        p1 = p1.clamp(0, lmt)
+
+        v0 = self.integral(param, p0)
+        v1 = self.integral(param, p1)
+
+        return (1 - pos) * v0 + pos * v1
+
+    @th.compile
+    def forward(self: U, data: Tensor) -> Tensor:
+        if self.theta.device != data.device:
+            self.theta = self.theta.to(data.device)
+            self.velocity = self.velocity.to(data.device)
         shape = data.size()
-        data_ = data.flatten(0)
-        param_ = param.flatten(0)
+        data = (data - data.mean()) / data.std() * self.iscale
+        data = data.flatten(0)
 
-        index = th.tanh(data_) * (points - 1)
-        frame = param_.to(data_.device())
+        theta = self.interplot(self.theta, th.sigmoid(data) * (self.points - 1))
+        ds = self.interplot(self.velocity, th.abs(th.tanh(data) * (self.points - 1)))
 
-        begin = index.floor().long()
-        begin = begin.clamp(0, param.size(1) - 1)
-        pos = index - begin
-        end = begin + 1
-        end = end.clamp(0, param.size(1) - 1)
-
-        result = (1 - pos) * frame[begin] + pos * frame[end]
-
-        return result.view(*shape)
-
-    def foilize(self: U, data: Tensor, param: Tensor) -> Tensor:
-
-        theta = self.by_tanh(param[0:1], data)
-        velo = self.by_tanh(param[1:2], data)
-        ds = velo * 0.01
         dx = ds * th.cos(theta)
         dy = ds * th.sin(theta)
-        val = data * (1 + dy) + dx
-        return val
+        data = data * th.exp(dy) + dx
 
-    def forward(self: U, data: Tensor) -> Tensor:
-        shape = data.size()
-        data = data.contiguous()
         data = (data - data.mean()) / data.std()
-
-        trunk = []
-        params = self.params * th.ones_like(self.params)
-        for ix in range(self.groups):
-            data_slice = data[:, ix::self.groups].reshape(-1, 1, 1)
-            param_slice = params[:, ix:ix+1]
-            trunk.append(self.foilize(data_slice, param_slice))
-        data = th.cat(trunk, dim=1)
-
-        return data.view(*shape)
+        return data.view(*shape) * self.oscale
 
 
 class Cifar0(CIFARModel):
     def __init__(self):
         super().__init__()
         self.conv0 = nn.Conv2d(3, 135, kernel_size=7, padding=3)
-        self.fiol0 = Foil(groups=1, points=3)
+        self.fiol0 = LNon()
         self.conv1 = nn.Conv2d(135, 405, kernel_size=3, padding=1)
-        self.fiol1 = Foil(groups=1, points=3)
-        self.conv2 = nn.Conv2d(405, 405, kernel_size=1, padding=0)
-        self.fiol2 = Foil(groups=1, points=3)
-        self.conv3 = nn.Conv2d(405, 405, kernel_size=1, padding=0)
-        self.fiol3 = Foil(groups=1, points=3)
-        self.fc = nn.Linear(135 * 16, 100)
+        self.fiol1 = LNon()
+        self.conv2 = nn.Conv2d(405, 405, kernel_size=3, padding=1)
+        self.fiol2 = LNon()
+        self.conv3 = nn.Conv2d(405, 405, kernel_size=3, padding=1)
+        self.fiol3 = LNon()
+        self.fc = nn.Linear(405 * 16, 100)
 
     def forward(self, x):
         x = self.conv0(x)
